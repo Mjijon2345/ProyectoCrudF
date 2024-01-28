aba580e6eaf44498202e1b887d7c4caf1fd70042
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2024-01-28 18:03:00 -0500

    Usuario listo

--- Modelos/Usuario.cs
+++ Modelos/Usuario.cs
@@ -1,15 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
+// Espacio de nombres que define la estructura de modelos de datos
 namespace ProyectoCrudF.Modelos
 {
+    // Clase que representa el modelo de datos para un usuario
     public class Usuario
     {
+        // Atributo clave primaria que representa el identificador único del usuario
         [Key]
         public int IdUsuario { get; set; }
-        public string NombreCompleto { get; set; }
-        public string Correo { get; set; }
-        public int Telefono { get; set; }
-        public DateTime Fecha { get; set; }
 
+        // Atributo que representa el nombre completo del usuario
+        public string NombreCompleto { get; set; }
+
+        // Atributo que representa la dirección de correo electrónico del usuario
+        public string Correo { get; set; }
+
+        // Atributo que representa el número de teléfono del usuario
+        public int Telefono { get; set; }
+
+        // Atributo que representa la fecha asociada al usuario
+        public DateTime Fecha { get; set; }
     }
 }
